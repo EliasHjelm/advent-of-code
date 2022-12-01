9ac7d27dcd2726dc67f17d5a1a9bce024f7e7b88
Elias Hjelm <elias@goboat.dk>
2022-12-01 08:42:00 +0100

    part 20 2021

---
+++ 2021/20/part-1.js
@@ -0,0 +1,20 @@
+const range = require('../../utils/range');
+const combinations = require('../../utils/combinations');
+
+let input
+input = require('fs').readFileSync(require.resolve('./input'), 'utf-8');
+// input = require('fs').readFileSync(require.resolve('./t1'), 'utf-8');
+// input = require('fs').readFileSync(require.resolve('./t2'), 'utf-8');
+
+
+// remove newline at end of file
+const lines = input.split('\n').slice(0, -1);
+console.log('input', lines);
+
+const numbers = lines.map(Number);
+
+let iterations = 10;
+
+for (let i = 0; i < iterations; i++) {
+
+}
